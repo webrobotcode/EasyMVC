7f2dbb72dc32d3d1ca8b58f292119a8bfd241b8c
Sakib Hasan <mdaynulhuq6@gmail.com>
2020-10-08 03:35:21 +0600

    EasyMVC Folders Commit

---
+++ core/Application.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * EasyMVC Core Namespace
+ */
+namespace EasyMVC\Core;
+
+/**
+ * Application Class
+ */
+class Application
+{
+    /**
+     * Construct method
+     */
+    public function __construct()
+    {
+        echo "I'm Application";
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * EasyMVC Framework
+ * 
+ * Easymvc is an opensource project. You can implement it. It's only for educational 
+ * purpose. Thank you
+ * 
+ * @category  EasyMVC
+ * @author    Sakib Hasan <shakibdhaka.me@outlook.com>
+ * @license   GNU General Public License v3.0 https://www.easymvc.com/license
+ * @copyright 2020 The Author
+ */
+
+/**
+ * Require autoload
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Import classes
+ */
+use EasyMVC\Core\Application;
+
+$app = new Application();
